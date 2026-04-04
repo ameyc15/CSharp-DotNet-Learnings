ccb6249fdd4dd1881f0dc7617656f42837254d69
ameyc15 <ameychaudhary@yahoo.com>
2026-04-05 00:05:01 +0530

    Day01-Added HelloWorld Console Project with Classic Main Method

---
+++ CSharp-DotNet-Masterclass/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My C# Learning Journey Starts Today!");
+        }
+    }
+}
