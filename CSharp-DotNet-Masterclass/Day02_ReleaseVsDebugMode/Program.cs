d0a64d835a6f5bb9346653e8eff06c38574e3f36
ameyc15 <ameychaudhary@yahoo.com>
2026-04-05 16:06:54 +0530

    Day02-Release and Debug Mode Experiment

---
+++ CSharp-DotNet-Masterclass/Day02_ReleaseVsDebugMode/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace Day02_ReleaseVsDebugMode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===== Debug vs Release Mode Experiment ===== \n");
+            Console.WriteLine("=== This program will be run by Release Mode and Debug Mode ===\n");
+            Console.WriteLine("=== Open the project in file explorer and compare the size of dlls made by both modes ===\n");
+            Console.WriteLine("=== dll made by Debug mode should be larger in size than dll made by Release Mode === \n");
+
+            // 1 . We are going to do Matrix Multiplication which is CPU Intensive for the local machine
+            Console.WriteLine("1. Running Matrix Multiplication...");
+            int[,] result = MultiplyMatrices();
+            Console.WriteLine($"Matrix result [0,0] = {result[0, 0]}");
+            Console.WriteLine($"Matrix result [1,1] = {result[1, 1]}");
+            Console.WriteLine($"Matrix result [2,2] = {result[2, 2]}");
+            Console.WriteLine($"Matrix result [3,3] = {result[3, 3]}");
+
+
+            //2 . We will be count the prime numbers 
+            Console.WriteLine("2. Finding the Prime Numbers from 0 to 100000");
+            int primeCount = CountPrimes(100000);
+            Console.WriteLine($"Total Prime Numbers from 0 to 100000 are : {primeCount}");
+
+
+        }
+
+        // 1. Method to implement the Matrix Multiplication
+        static int[,] MultiplyMatrices()
+        {
+            int size = 10;
+            int[,] MatrixA = new int[size, size];
+            int[,] MatrixB = new int[size, size];
+            int[,] MatrixC = new int[size, size];
+
+            //We will populate the matrices 
+            for (int i = 0; i < size; i++)
+            {
+                for (int j=0; j<size; j++)
+                {
+                    MatrixA[i, j] = i + j;
+                    MatrixB[i, j] = i * j;
+                }
+            }
+            Console.WriteLine(MatrixA);
+            Console.WriteLine(MatrixB);
+
+            //Mutliplication of Matrices
+            for (int i=0; i<size; i++)
+            {
+                for(int j=0; j<size;j++)
+                {
+                    for(int k=0; k<size; k++)
+                    {
+                        MatrixC[i, j] += MatrixA[i, k] * MatrixB[k, j];
+                    }
+                }
+            }
+            return MatrixC;
+        }
+        
+        //Method to Count the Prime Numbers upto 100000
+        static int CountPrimes(int limit)
+        {
+            int count = 0;
+
+            for(int num =2; num<=limit; num++)
+            {
+                bool isPrime = true;
+
+                for(int i=2; i<= Math.Sqrt(num); i++)
+                {
+                    if(num % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime) count++;
+            }
+            return count; 
+        }
+
+
+    }
+}
