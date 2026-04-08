70d035b216473f96d85a560801dd10c353eacd59
ameyc15 <ameychaudhary@yahoo.com>
2026-04-08 15:11:21 +0530

    Day03-Added Notes in Comments

--- CSharp-DotNet-Masterclass/Day03-MainMethod/Program.cs
+++ CSharp-DotNet-Masterclass/Day03-MainMethod/Program.cs
@@ -1,7 +1,10 @@
 ﻿namespace Day03_MainMethod
 {
+    //the class is internal so it can not be access outside of the dll
     internal class Program
-    {
+    {   // main method is always static so in order to invoke the method we don't need to
+        // create the object. This is entry point of program. In order to run the program first thing 
+        // CLR will do is to find a method names Main.
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!"); 
