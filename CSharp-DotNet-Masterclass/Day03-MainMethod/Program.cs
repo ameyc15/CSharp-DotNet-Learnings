bc597eba486b9b65de7350b5665bc53d31a48006
ameyc15 <ameychaudhary@yahoo.com>
2026-04-08 14:59:56 +0530

    Day03-Main method

---
+++ CSharp-DotNet-Masterclass/Day03-MainMethod/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day03_MainMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); 
+        }
+    }
+}
