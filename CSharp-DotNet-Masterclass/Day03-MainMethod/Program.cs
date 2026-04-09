e02dbfa781813055e05ab93d6fbb26878c1f56f1
ameyc15 <ameychaudhary@yahoo.com>
2026-04-09 12:03:04 +0530

    Day03-Size of Integers

--- CSharp-DotNet-Masterclass/Day03-MainMethod/Program.cs
+++ CSharp-DotNet-Masterclass/Day03-MainMethod/Program.cs
@@ -7,7 +7,14 @@
         // CLR will do is to find a method names Main.
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!"); 
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("====== Size of all the Value Types (in Bytes) =====");
+            //Integers
+            Console.WriteLine($"Size of Byte : {sizeof(byte)} byte");
+            Console.WriteLine($"Size of Short : {sizeof(short)} bytes");
+            Console.WriteLine($"Size of Int : {sizeof(int)} bytes");
+            Console.WriteLine($"Size of Long : {sizeof(long)} bytes");
         }
     }
 }
