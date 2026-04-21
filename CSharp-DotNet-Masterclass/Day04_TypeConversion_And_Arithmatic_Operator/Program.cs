b13c3ee528beba161669d7f44c38f0665982002b
ameyc15 <ameychaudhary@yahoo.com>
2026-04-21 11:55:19 +0530

    Implicit Type Casting

--- CSharp-DotNet-Masterclass/Day04_TypeConversion_And_Arithmatic_Operator/Program.cs
+++ CSharp-DotNet-Masterclass/Day04_TypeConversion_And_Arithmatic_Operator/Program.cs
@@ -7,13 +7,20 @@
         //takes input array of strings 
         // returns nothing
         static void Main(string[] args)
-        {           
-            // Explicit Type Casting
+        {
+            //Implicit Type Casting => Converting Smaller Data Type to Larger Data type (Automatic), No Data Loss
+            int myVar = 10;
+            double myVarDouble = myVar;
+            Console.WriteLine(myVar);  // prints 10
+            Console.WriteLine(myVarDouble); // also  prints 10
+
+
+            // Explicit Type Casting => when converting larger data type to smaller might be data loss
             // double -> int -> char
             double valuePi = 3.143;
             int valuePiInt = (int)valuePi;
-            Console.WriteLine("Variable Type Double valuePi : "+valuePi);
-            Console.WriteLine("Varibale Type Int valuePiInt : "+valuePiInt);
+            Console.WriteLine("Variable Type Double valuePi : "+valuePi); // prints 3.143
+            Console.WriteLine("Varibale Type Int valuePiInt : "+valuePiInt); // only pritns 3 => data loss after decimal because memory size
         }
         
     }
