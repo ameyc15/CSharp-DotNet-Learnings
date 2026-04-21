f51d22cfa6500216ced39a82b754194fcfbf4b49
ameyc15 <ameychaudhary@yahoo.com>
2026-04-21 11:50:29 +0530

    Explicit TypeCasting

---
+++ CSharp-DotNet-Masterclass/Day04_TypeConversion_And_Arithmatic_Operator/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Day04_TypeConversion_And_Arithmatic_Operator
+{
+   internal class Program
+    {
+        // summary
+        // Main method is execution starting point and contains flow of execution
+        //takes input array of strings 
+        // returns nothing
+        static void Main(string[] args)
+        {           
+            // Explicit Type Casting
+            // double -> int -> char
+            double valuePi = 3.143;
+            int valuePiInt = (int)valuePi;
+            Console.WriteLine("Variable Type Double valuePi : "+valuePi);
+            Console.WriteLine("Varibale Type Int valuePiInt : "+valuePiInt);
+        }
+        
+    }
+}
