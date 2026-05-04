76815a7e47561add759cd52196ce38bc8db8fddd
ameyc15 <ameychaudhary@yahoo.com>
2026-05-04 13:23:04 +0530

    area of circle

---
+++ CSharp-DotNet-Masterclass/Day05_Conditional_Statements/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Day05_Conditional_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            // write a program to calculate the area of circle based on given value of radius
+
+            Console.WriteLine("Enter the radius of circle ");
+
+            string input = Console.ReadLine();
+            double radiusOfCricle = Convert.ToDouble(input);
+
+            double areaOfCircle = 3.14 * radiusOfCricle * radiusOfCricle;
+
+            Console.WriteLine($"Area of Circle is : {areaOfCircle}");
+        }
+    }
+}
