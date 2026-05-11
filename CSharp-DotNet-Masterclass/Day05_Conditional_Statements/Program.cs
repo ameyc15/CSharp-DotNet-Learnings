5ec3f539b88c797c2cb08a13d4e6599fa02b9632
ameyc15 <ameychaudhary@yahoo.com>
2026-05-11 20:06:52 +0530

    Conditional Statement Example

--- CSharp-DotNet-Masterclass/Day05_Conditional_Statements/Program.cs
+++ CSharp-DotNet-Masterclass/Day05_Conditional_Statements/Program.cs
@@ -23,6 +23,32 @@
             double areaOfCircle = pi * radiusOfCircle * radiusOfCircle;
 
             Console.WriteLine($"Area of Circle is : {areaOfCircle:F2}");
+
+            // Write a program to find out "category of height" based on the given height of a person (in inches), using "if".
+            Console.WriteLine("Please enter height of Person in Inches");
+            float heightInInches;
+            while(!float.TryParse(Console.ReadLine(), out heightInInches))
+            {
+                Console.WriteLine("Invalid Input. Please Enter Height of Person in Inches");
+
+            }
+            double heightInCm = 2.54 * heightInInches;
+            if (heightInCm < 150)
+            {
+                Console.WriteLine("Person is Dwarf");
+            }
+            else if (heightInCm < 165)
+            {
+                Console.WriteLine("Person is of Average Height");
+            }
+            else if(heightInCm < 195)
+            {
+                Console.WriteLine("Person is tall");
+            }
+            else
+            {
+                Console.WriteLine("Person is of abnormal Height");
+            }
         }
     }
 }
