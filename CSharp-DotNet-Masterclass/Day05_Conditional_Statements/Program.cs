44e6fdfdce771de435a132704097a80fb77d827f
ameyc15 <ameychaudhary@yahoo.com>
2026-05-21 16:18:22 +0530

    Final Day05 Assignment

--- CSharp-DotNet-Masterclass/Day05_Conditional_Statements/Program.cs
+++ CSharp-DotNet-Masterclass/Day05_Conditional_Statements/Program.cs
@@ -4,177 +4,192 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-
-            Console.WriteLine("***********Program has Stared***********");
-            Console.WriteLine("Program to Calculate the circle of Radius");
-            AreaOfCircle();
-            Console.WriteLine("\n...................................................");
-
-            Console.WriteLine("Program to Categorise Height");
-            CategoryOfHeight();
-            Console.WriteLine("\n...................................................");
-
-            Console.WriteLine("Prgram to calculate Largest Number using if else statements ");
-            LargestNumber();
-            Console.WriteLine("\n......................................................");
-
-            Console.WriteLine("Prgram to Calculate Largest Number Refactored Above Method");
-            LargestNum();
-            Console.WriteLine("\n...................................................");
-
-            Console.WriteLine("Prgram to Calculate Largest Number using Max function");
-            MaxNumnber();
-            Console.WriteLine("\n.....................................................");
-            
-            // write a program to calculate the area of circle based on given value of radius
-            static void AreaOfCircle()
             {
-                double pi = Math.PI;
-                Console.WriteLine("Please enter the radius of circle : ");
 
-                // string input = Console.ReadLine();
 
-                double radiusOfCircle;
+                Console.WriteLine("***********Program has Stared***********");
+                Console.WriteLine("Program to Calculate the circle of Radius");
+                AreaOfCircle();
+                Console.WriteLine("\n...................................................");
 
-                while (!double.TryParse(Console.ReadLine(), out radiusOfCircle))
+                Console.WriteLine("Program to Categorise Height");
+                CategoryOfHeight();
+                Console.WriteLine("\n...................................................");
+
+                Console.WriteLine("Prgram to calculate Largest Number using if else statements ");
+                LargestNumber();
+                Console.WriteLine("\n......................................................");
+
+                Console.WriteLine("Prgram to Calculate Largest Number Refactored Above Method");
+                LargestNum();
+                Console.WriteLine("\n...................................................");
+
+                Console.WriteLine("Prgram to Calculate Largest Number using Max function");
+                MaxNumnber();
+                Console.WriteLine("\n.....................................................");
+
+                ///<summary>
+                /// Problem 1: Calculates the area of a circle based on user-provided radius.
+                ///</summary>
+                static void AreaOfCircle()
                 {
-                    Console.WriteLine("Invalid Value for Radius. Please Enter Valid Numeric Value");
+                    double pi = Math.PI;
+                    Console.WriteLine("Please enter the radius of circle : ");
 
-                }
+                    // string input = Console.ReadLine();
 
-                double areaOfCircle = pi * radiusOfCircle * radiusOfCircle;
+                    double radiusOfCircle;
 
-                Console.WriteLine($"Area of Circle is : {areaOfCircle:F2}");
-            }
-
-            // Write a program to find out "category of height" based on the given height of a person (in inches), using "if".
-            static void CategoryOfHeight()
-            {           
-            Console.WriteLine("Please enter height of Person in Inches");
-            float heightInInches;
-            while(!float.TryParse(Console.ReadLine(), out heightInInches))
-            {
-                Console.WriteLine("Invalid Input. Please Enter Height of Person in Inches");
-
-            }
-            double heightInCm = 2.54 * heightInInches;
-            if (heightInCm < 150)
-            {
-                Console.WriteLine("Person is Dwarf");
-            }
-            else if (heightInCm < 165)
-            {
-                Console.WriteLine("Person is of Average Height");
-            }
-            else if(heightInCm < 195)
-            {
-                Console.WriteLine("Person is tall");
-            }
-            else
-            {
-                Console.WriteLine("Person is of abnormal Height");
-            }
-           }
-
-            // Write a C# program to find out largest number among three numbers, using "if".
-            void LargestNumber()
-            {
-                int firstNumber, secondNumber, thirdNumber;
-
-
-                while (!int.TryParse(Console.ReadLine(), out firstNumber))
-                {
-                    Console.WriteLine("Invalid Integer...Please Enter Valid Integer Number");
-                }
-                Console.WriteLine("Your Fisrt Number is " + firstNumber);
-                while (!int.TryParse(Console.ReadLine(), out secondNumber))
-                {
-                    Console.WriteLine("Innvalid Integer.. Please Enter Second Valid Whole Number");
-                }
-                Console.WriteLine("Your Second Number is" + secondNumber);
-                while (!int.TryParse(Console.ReadLine(), out thirdNumber))
-                {
-                    Console.WriteLine("Invalid Input... Please Enter the Third Valid Whole Number");
-                }
-                Console.WriteLine("Your Third Number is " + thirdNumber);
-
-                if (firstNumber > secondNumber)
-                {
-                    if (firstNumber > thirdNumber)
+                    while (!double.TryParse(Console.ReadLine(), out radiusOfCircle))
                     {
-                        Console.WriteLine($"Largest Number is : {firstNumber}");
+                        Console.WriteLine("Invalid Value for Radius. Please Enter Valid Numeric Value");
+
+                    }
+
+                    double areaOfCircle = pi * radiusOfCircle * radiusOfCircle;
+
+                    Console.WriteLine($"Area of Circle is : {areaOfCircle:F2}");
+                }
+
+                //<summary>
+                /// Problem 2: Categorizes a person's height after converting inches to centimeters.
+                //</summary>
+                static void CategoryOfHeight()
+                {
+                    Console.WriteLine("Please enter height of Person in Inches");
+                    float heightInInches;
+                    while (!float.TryParse(Console.ReadLine(), out heightInInches))
+                    {
+                        Console.WriteLine("Invalid Input. Please Enter Height of Person in Inches");
+
+                    }
+                    double heightInCm = 2.54 * heightInInches;
+                    if (heightInCm < 150)
+                    {
+                        Console.WriteLine("Person is Dwarf");
+                    }
+                    else if (heightInCm < 165)
+                    {
+                        Console.WriteLine("Person is of Average Height");
+                    }
+                    else if (heightInCm < 195)
+                    {
+                        Console.WriteLine("Person is tall");
                     }
                     else
                     {
-                        Console.WriteLine($"Largest Number is : {thirdNumber}");
+                        Console.WriteLine("Person is of abnormal Height");
                     }
                 }
-                else
+
+                /// <summary>
+                /// Write a C# program to find out largest number among three numbers, using "if".
+                /// </summary>
+                void LargestNumber()
                 {
-                    if (secondNumber > thirdNumber)
+                    int firstNumber, secondNumber, thirdNumber;
+
+
+                    while (!int.TryParse(Console.ReadLine(), out firstNumber))
                     {
-                        Console.WriteLine($"Largest Number is : {secondNumber}");
+                        Console.WriteLine("Invalid Integer...Please Enter Valid Integer Number");
+                    }
+                    Console.WriteLine("Your Fisrt Number is " + firstNumber);
+                    while (!int.TryParse(Console.ReadLine(), out secondNumber))
+                    {
+                        Console.WriteLine("Innvalid Integer.. Please Enter Second Valid Whole Number");
+                    }
+                    Console.WriteLine("Your Second Number is" + secondNumber);
+                    while (!int.TryParse(Console.ReadLine(), out thirdNumber))
+                    {
+                        Console.WriteLine("Invalid Input... Please Enter the Third Valid Whole Number");
+                    }
+                    Console.WriteLine("Your Third Number is " + thirdNumber);
+
+                    if (firstNumber > secondNumber)
+                    {
+                        if (firstNumber > thirdNumber)
+                        {
+                            Console.WriteLine($"Largest Number is : {firstNumber}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Largest Number is : {thirdNumber}");
+                        }
                     }
                     else
                     {
-                        Console.WriteLine($"Largest Number is : {thirdNumber}");
+                        if (secondNumber > thirdNumber)
+                        {
+                            Console.WriteLine($"Largest Number is : {secondNumber}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Largest Number is : {thirdNumber}");
+                        }
                     }
                 }
-            }
-            //Write a C# program to find out largest number among three numbers, using "if"
-            void LargestNum()
-            {
-                // This is more advanced solution compared to above one
-                int firstNum, secondNum, thirdNum, largest;
-                Console.WriteLine("Enter theree Numbers ");
-                while (!int.TryParse(Console.ReadLine(), out firstNum))
+
+                /// <summary>
+                /// Write a C# program to find out largest number among three numbers, using "if"
+                /// </summary>
+                void LargestNum()
                 {
-                    Console.WriteLine("Invalid Input...Enter Whole Number");
-                }
-                while (!int.TryParse(Console.ReadLine(), out secondNum))
-                {
-                    Console.WriteLine("Invalid Input...Enter Whole Number");
-                }
-                while (!int.TryParse(Console.ReadLine(), out thirdNum))
-                {
-                    Console.WriteLine("Invalid Input...Enter Whole Number");
-                }
-                Console.WriteLine("First Number is " + firstNum);
-                Console.WriteLine("Second Number is " + secondNum);
-                Console.WriteLine("Third Number is " + thirdNum);
-                if (firstNum >= secondNum && firstNum >= thirdNum)
-                {
-                    largest = firstNum;
-                }
-                else if (secondNum >= firstNum && secondNum >= thirdNum)
-                {
-                    largest = secondNum;
-                }
-                else
-                {
-                    largest = thirdNum;
-                }
-                Console.WriteLine("Largest Nummber is :" + largest);
-            }
-            void MaxNumnber()
-            {
-                int[] numbers = new int[3];
-                Console.WriteLine("Enter Three Numbers");
-                for (int i = 0; i < numbers.Length; i++)
-                {
-                    Console.WriteLine($"Enter your {i + 1}st Number");
-                    while (!int.TryParse(Console.ReadLine(), out numbers[i]))
+                    // This is more advanced solution compared to above one
+                    int firstNum, secondNum, thirdNum, largest;
+                    Console.WriteLine("Enter theree Numbers ");
+                    while (!int.TryParse(Console.ReadLine(), out firstNum))
                     {
-                        Console.WriteLine("Invalid Input... Please Enter Whole Number");
+                        Console.WriteLine("Invalid Input...Enter Whole Number");
                     }
+                    while (!int.TryParse(Console.ReadLine(), out secondNum))
+                    {
+                        Console.WriteLine("Invalid Input...Enter Whole Number");
+                    }
+                    while (!int.TryParse(Console.ReadLine(), out thirdNum))
+                    {
+                        Console.WriteLine("Invalid Input...Enter Whole Number");
+                    }
+                    Console.WriteLine("First Number is " + firstNum);
+                    Console.WriteLine("Second Number is " + secondNum);
+                    Console.WriteLine("Third Number is " + thirdNum);
+                    if (firstNum >= secondNum && firstNum >= thirdNum)
+                    {
+                        largest = firstNum;
+                    }
+                    else if (secondNum >= firstNum && secondNum >= thirdNum)
+                    {
+                        largest = secondNum;
+                    }
+                    else
+                    {
+                        largest = thirdNum;
+                    }
+                    Console.WriteLine("Largest Nummber is :" + largest);
                 }
-                Console.WriteLine($"Your Numbers are : {string.Join(", ", numbers)}");
-                int largestN = numbers.Max();
-                Console.WriteLine("Largest Number is :" + largestN);
+
+                /// <summary>
+                /// Finding the max number using in-built functions
+                /// </summary>
+                void MaxNumnber()
+                {
+                    int[] numbers = new int[3];
+                    Console.WriteLine("Enter Three Numbers");
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        Console.WriteLine($"Enter your {i + 1}st Number");
+                        while (!int.TryParse(Console.ReadLine(), out numbers[i]))
+                        {
+                            Console.WriteLine("Invalid Input... Please Enter Whole Number");
+                        }
+                    }
+                    Console.WriteLine($"Your Numbers are : {string.Join(", ", numbers)}");
+                    int largestN = numbers.Max();
+                    Console.WriteLine("Largest Number is :" + largestN);
+                }
+
+
             }
-
-
         }
     }
 }
