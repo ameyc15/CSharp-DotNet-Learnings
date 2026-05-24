8bfd49550b7a9f52b273d16afb82041d19ccba89
ameyc15 <ameychaudhary@yahoo.com>
2026-05-24 23:14:48 +0530

    oops start

---
+++ CSharp-DotNet-Masterclass/Day08_IntroToOOPS/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace Day07_IntroToOOPS
+{
+    public class Person
+    {
+        public string name = "Amey";
+        public int age = 31;
+
+        public void Greet()
+        {
+            Console.WriteLine("Greet method invoked");
+        }
+    }
+}
---
+++ CSharp-DotNet-Masterclass/Day08_IntroToOOPS/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Day07_IntroToOOPS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello  World");
+
+            // create a class named person with name and age as fields and method greet
+
+            Person p1 = new Person();
+
+            p1.Greet();
+            Console.WriteLine(p1.age);
+            Console.WriteLine(p1.name);
+        }
+    }
+}
