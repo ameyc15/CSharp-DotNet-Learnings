7fb450ce2138a559ad2f15fc436caaf02b4d435a
ameyc15 <ameychaudhary@yahoo.com>
2026-06-14 23:22:17 +0530

    Day_10_Start

---
+++ CSharp-DotNet-Masterclass/Day10_IntroToConstructor/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day10_IntroToConstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
