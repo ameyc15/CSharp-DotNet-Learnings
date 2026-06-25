8ba0e22143e5f2229c117d1812b8cff0cde82467
ameyc15 <ameychaudhary@yahoo.com>
2026-06-25 12:31:10 +0530

    Constuctor

---
+++ CSharp-DotNet-Masterclass/Day10_IntroToConstructor/BankAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class BankAccount
+    {
+        public double balance { get; set; }
+        public string bankName { get; set; }
+
+        // Constructor :  Special Method with no return type but same name as class name
+        // Uses : 1. Used to initialise the fields 
+        public BankAccount()
+        {
+            Console.WriteLine("Constructor without parameter invoked");
+            balance = 1000;
+        }
+        // Parammeterised Constructor will be invoked using BankAccount acc1 = new BankAccount(300);
+        public BankAccount(double amount)
+        {
+            Console.WriteLine("Constructor with one parameter invoked");
+            balance = amount;
+        }
+        public BankAccount(string bankname, double amount)
+        {
+            Console.WriteLine("Constructor with two Parameter invoked");
+            bankName = bankname;
+            balance = amount;
+        }
+    }
+}
--- CSharp-DotNet-Masterclass/Day10_IntroToConstructor/Program.cs
+++ CSharp-DotNet-Masterclass/Day10_IntroToConstructor/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Day10_IntroToConstructor
+﻿using Bank;
+
+namespace Day10_IntroToConstructor
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            // namespace is  collection of classes or interfaces clubed together
+            // 
+            BankAccount account;    // only reference variable created as account no memeory allocated 
+            BankAccount account1 = new BankAccount();   // cotr with no parameter invoked
+            BankAccount account2 = new BankAccount(2000);   // cotr with 1 parameter invoked
+            BankAccount account3 = new BankAccount("AXIS", 2000); // cotr with 2 parameter invoked
         }
     }
 }
