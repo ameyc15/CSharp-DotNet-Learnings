38cd8ae4f24ab090970c81a418e5c7c7b42859b5
ameyc15 <ameychaudhary@yahoo.com>
2026-06-25 18:57:05 +0530

    New

---
+++ CSharp-DotNet-Masterclass/Day11_MethodPracticeAndOverloading/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Day11_MethodPracticeAndOverloading
+{
+    internal class Program
+    {
+        public static void CityMethod(string city = "Pune")
+        {
+            Console.WriteLine(city);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            CityMethod();
+            CityMethod("Mumbai");        }
+    }
+}
