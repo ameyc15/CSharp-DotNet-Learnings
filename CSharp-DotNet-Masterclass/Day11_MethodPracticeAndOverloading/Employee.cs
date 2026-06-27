03536a440bc0631e0675349d6bcc48fa2170783d
ameyc15 <ameychaudhary@yahoo.com>
2026-06-27 13:37:21 +0530

    Problem & Solution

---
+++ CSharp-DotNet-Masterclass/Day11_MethodPracticeAndOverloading/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11_MethodPracticeAndOverloading
+{
+    internal class Employee
+    {
+        public int salary;
+        public string name = string.Empty;
+
+        public Employee()
+        {
+            Console.WriteLine("Employee Class Object Initialised using constructor");
+        }
+    }
+}
--- CSharp-DotNet-Masterclass/Day11_MethodPracticeAndOverloading/Program.cs
+++ CSharp-DotNet-Masterclass/Day11_MethodPracticeAndOverloading/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Day11_MethodPracticeAndOverloading
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Day11_MethodPracticeAndOverloading
 {
     internal class Program
     {
@@ -7,16 +9,15 @@
             Console.WriteLine(city);
             Console.WriteLine(population);
         }
-        public static int Sum(int x, int y)
+        public static int Sum(int x, int y) // method overloading
         {
             return x + y;
         }
-        static int Method(int x)
+        public static int Sum(int x, int y, int z) // method overloading
         {
-            return x;
-            Console.WriteLine();
+            return x + y + z;
         }
-         
+        public static string employeeName = "Amey";
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
@@ -25,6 +26,62 @@
 
             int output=Sum(100, 200);
             Console.WriteLine(output);
+
+            int sum1 = Sum(1,2);
+            Console.WriteLine(sum1);
+
+            int sum2 = Sum(1,2,3);
+            Console.WriteLine(sum2);
+
+            string cityName = "Ahilyanagr";
+            Console.WriteLine(cityName);
+            Console.WriteLine(employeeName);
+
+            // Write a class which will contain employee name and salary
+            // create 3 objects of class 
+            // 1. Peter 200
+            // 2. Rob 50
+            // 3. Donna 100
+            // print the name of the employee with highest salary
+
+            Employee employee1, employee2, employee3;
+            employee1 = new Employee();
+            employee1.name = "Peter";
+            employee1.salary = 200;
+
+            employee2 = new Employee();
+            employee2.name = "Rob";
+            employee2.salary = 500;
+
+            employee3 = new Employee();
+            employee3.name = "Donna";
+            employee3.salary = 1000;
+
+            // find the maximum salary 
+            if(employee1.salary > employee2.salary) 
+            {
+                if(employee1.salary > employee3.salary)
+                {
+                    Console.WriteLine("Peter has the highest salary");
+                }
+                else
+                {
+                    Console.WriteLine("Donna has the highest salary");
+                }
+            }
+            else
+            {
+                if (employee3.salary > employee2.salary)
+                {
+                    Console.WriteLine("Donna has the highest salary");
+                }
+                else
+                {
+                    Console.WriteLine("Rob has the highest salary");
+                }
+            }
+            string companyName = "newgen";
+            Console.WriteLine(companyName.ToUpper());
         }
     }
 }
