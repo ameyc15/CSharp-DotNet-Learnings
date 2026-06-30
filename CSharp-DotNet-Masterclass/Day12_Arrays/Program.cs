d4bb18c13e3fa1ba650830c2e1bf7e74d898df6c
ameyc15 <ameychaudhary@yahoo.com>
2026-06-30 14:53:06 +0530

    Max Number in Array

--- CSharp-DotNet-Masterclass/Day12_Arrays/Program.cs
+++ CSharp-DotNet-Masterclass/Day12_Arrays/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Day12_Arrays
+﻿namespace Day12_Arrays_Static_PartialClass
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] numbers = new int[5] { 1, 2, 3, 4, 5 };
+            Console.WriteLine(numbers[0]);
+
+            // Find the maximum number in the array
+
+            int maxNumber = int.MinValue;
+            Console.WriteLine(maxNumber);
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if(maxNumber<=numbers[i])
+                    maxNumber = numbers[i];
+            }
+            Console.WriteLine(maxNumber);
         }
     }
 }
