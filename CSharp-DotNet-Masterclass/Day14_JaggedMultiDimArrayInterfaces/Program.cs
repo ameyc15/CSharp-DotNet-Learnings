e9aca227599d0bbc95ef066f673c576fb271b194
ameyc15 <ameychaudhary@yahoo.com>
2026-07-15 00:27:24 +0530

    Started Day14 - Duplicates & Non Duplicates

---
+++ CSharp-DotNet-Masterclass/Day14_JaggedMultiDimArrayInterfaces/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Day14_JaggedMultiDimArrayInterfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            // Find the duplicate number in Array : {3,8,,2,,1,5,3}
+            // we will use nested loops
+            int[] arrOne = { 3, 8, 2, 1, 5, 5 };
+            int duplicateNumber = 0;
+            bool found = false;
+            for(int i=0; i<arrOne.Length-1; i++)
+            {
+                for(int j =i+1; j < arrOne.Length; j++)
+                {
+                    if (arrOne[i] == arrOne[j])
+                    {
+                        duplicateNumber = arrOne[i];
+                        found = true;
+                        break;
+                    }
+                }
+                if(found)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(duplicateNumber);
+
+
+
+            // Find the non duplicate number in Array : {3,2 ,5,5,3}
+            // we will use xor operator 
+            int[] arr = { 2, 2, 5, 5, 3 };
+            int xorResult = 0;
+            for (int i = 0; i < arr.Length; i++) 
+            {
+                xorResult = xorResult ^ arr[i];
+            }
+            Console.WriteLine(xorResult);
+
+
+            //Sorting 
+
+
+        }
+    }
+}
