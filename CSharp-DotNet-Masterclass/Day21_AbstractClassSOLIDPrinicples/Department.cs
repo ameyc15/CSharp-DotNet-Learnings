6eb06111c9e490b979c651997ce868808ffa08ac
ameyc15 <ameychaudhary@yahoo.com>
2026-07-22 16:51:26 +0530

    Solid Princi

---
+++ CSharp-DotNet-Masterclass/Day21_AbstractClassSOLIDPrinicples/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day21_AbstractClassSOLIDPrinicples
+{
+    internal class Department
+    {
+        // All department related fields & methods only no other methods 
+        public string DepartmentName;
+
+        public string DepartmentId;
+
+        public Department() { }
+
+        public void GetDepartment() { }
+
+        public void GetDepartmentId() { }
+    }
+}
---
+++ CSharp-DotNet-Masterclass/Day21_AbstractClassSOLIDPrinicples/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Day21_AbstractClassSOLIDPrinicples
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // SOLID Prinicples
+            // S - Single Responsibility - Each class should have single responsibility
+            // O = Open Closed - Open for Extension CLosed for Modification
+            // L - Liskov Substitution
+            // I - Interface Prinnicple
+            // D - Dependency Inversion
+        }
+    }
+}
