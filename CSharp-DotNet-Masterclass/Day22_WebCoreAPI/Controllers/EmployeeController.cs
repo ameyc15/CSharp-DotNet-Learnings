aa7e9736a003f9e058c4438ef69861152fea941d
ameyc15 <ameychaudhary@yahoo.com>
2026-07-25 20:26:19 +0530

    .Net Core Web API Project

---
+++ CSharp-DotNet-Masterclass/Day22_WebCoreAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day22_WebCoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public string EmployeeName = "Amey Chaudhary";
+
+        public string EmmployeeCity = "Pune";
+
+        public EmployeeController()
+        {
+
+        }
+
+        [HttpGet("GetEmployeeName")]
+        public string GetEmployeeName() 
+        {
+            return EmployeeName;
+        }
+
+        [HttpGet("GetCityName")]
+        public string GetEmployeeCity() 
+        {
+            return EmmployeeCity;
+        }
+    }
+}
