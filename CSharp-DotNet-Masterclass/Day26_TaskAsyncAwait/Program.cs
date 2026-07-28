0cc813d03a61c5dbfde07d475851de90348e9a69
ameyc15 <ameychaudhary@yahoo.com>
2026-07-28 16:17:58 +0530

    await async

---
+++ CSharp-DotNet-Masterclass/Day26_TaskAsyncAwait/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Day26_TaskAsyncAwait
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Task<int> result = LongProcess(); // if i dont write await here thread will not wait for this methods execution to end
+            ShortProcess();
+
+            int value = await result;
+
+
+        }
+        static async Task<int> LongProcess()
+        {
+            Console.WriteLine("LongProcess Started ...");
+            await Task.Delay(4000);
+            Console.WriteLine("LongProcess Ended ...");
+            return 10;
+        }
+
+        static void ShortProcess()
+        {
+            Console.WriteLine("ShortProcess Started...");
+
+            Console.WriteLine("ShortProcess Ended ...");
+        }
+    }
+}
