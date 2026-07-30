e4f0173380cc4c55236fee6a9dc4fcee19b1e326
ameyc15 <ameychaudhary@yahoo.com>
2026-07-30 14:38:23 +0530

    Simple Health Check api

---
+++ CSharp-DotNet-Masterclass/Day27_HealthCheckAPIExample/Controllers/HealthCheckController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day27_HealthCheckAPIExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet("Health/Check")]
+        public IActionResult HealthCheck()
+        {
+            // check health status of server
+            return Ok("Health Good");
+        }
+    }
+}
---
+++ CSharp-DotNet-Masterclass/Day27_HealthCheckAPIExample/Controllers/PaymentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day27_HealthCheckAPIExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        // dummy api in payment service
+        [HttpGet("ProcessPayment")]
+        public IActionResult ProcessPayment()
+        { 
+            // do payment
+            return Ok("Payment successful");
+        }
+    }
+}
