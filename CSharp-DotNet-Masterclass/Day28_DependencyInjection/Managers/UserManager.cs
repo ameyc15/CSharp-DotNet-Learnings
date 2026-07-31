63cdd2c72179cf8e921eb3c0a086a40bed5b4bfd
ameyc15 <ameychaudhary@yahoo.com>
2026-07-31 07:04:36 +0530

    simple object creation using new keyword

--- CSharp-DotNet-Masterclass/Day28_DependencyInjection/Managers/UserManager.cs
+++ CSharp-DotNet-Masterclass/Day28_DependencyInjection/Managers/UserManager.cs
@@ -1,7 +1,23 @@
-﻿namespace Day28_DependencyInjection.Managers
+﻿using Day28_DependencyInjection.Repository;
+
+namespace Day28_DependencyInjection.Managers
 {
     public class UserManager
     {
+        public UserManager() { }
 
+        public void CreateUser()
+        {
+            // will invoke repository method
+            UserReposiotry userReposiotry = new UserReposiotry();
+            userReposiotry.AddUser();
+        }
+
+        public void RemoveUser()
+        {
+            // will invoke repository method
+            UserReposiotry userReposiotry = new UserReposiotry();
+            userReposiotry.DeleteUser();
+        }
     }
 }
--- CSharp-DotNet-Masterclass/Day28_DependencyInjection/Repository/UserReposiotry.cs
+++ CSharp-DotNet-Masterclass/Day28_DependencyInjection/Repository/UserReposiotry.cs
@@ -2,5 +2,15 @@
 {
     public class UserReposiotry
     {
+        public UserReposiotry() { }
+
+        public void AddUser()
+        {
+            Console.WriteLine("User Added to Repository");
+        }
+        public void DeleteUser()
+        {
+            Console.WriteLine("User Removed from Database");
+        }
     }
 }
