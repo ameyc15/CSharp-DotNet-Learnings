b65f6018db5d80fc073680f65723e3817d3958c5
ameyc15 <ameychaudhary@yahoo.com>
2026-08-01 00:23:42 +0530

    Linq -1

---
+++ CSharp-DotNet-Masterclass/Day30_LINQ_1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Day30_LINQ_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students =
+            {
+                new Student() {Age = 23, StudentName = "Amey"},
+                new Student() {Age = 27, StudentName = "Padam"},
+                new Student() {Age = 34, StudentName = "Tom"},
+                new Student() {Age = 29, StudentName = "Rahul"},
+                new Student() {Age = 25, StudentName = "Vinit"}
+            };
+
+            // 
+        }
+    }
+}
---
+++ CSharp-DotNet-Masterclass/Day30_LINQ_1/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day30_LINQ_1
+{
+    public class Student
+    {
+        public int Age { get; set; }
+
+        public string StudentName { get; set; }
+    }
+
+}
