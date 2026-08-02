7d99af8b6ccf3a2dfd7de939d4500c7f1bdb35d7
ameyc15 <ameychaudhary@yahoo.com>
2026-08-02 11:32:51 +0530

    a

--- CSharp-DotNet-Masterclass/Day30_LINQ_1/Program.cs
+++ CSharp-DotNet-Masterclass/Day30_LINQ_1/Program.cs
@@ -10,15 +10,30 @@
                 new Student() {Age = 27, StudentName = "Padam"},
                 new Student() {Age = 34, StudentName = "Tom"},
                 new Student() {Age = 29, StudentName = "Rahul"},
-                new Student() {Age = 25, StudentName = "Vinit"}
+                new Student() {Age = 25, StudentName = "Vinit"},
+                new Student() {Age = 19, StudentName = "Chiku" },
+                new Student() {Age = 20, StudentName = "Shubham"},
+                new Student() {Age = 21, StudentName = "Shreyas" }
             };
 
-            // if I want to fetch the students whose age is between 18 to 25 without using LINQ
+            // if I want to fetch the students whose age is between 15 to 25 without using LINQ
+            Student[] filteredStudents = new Student[10];
+            int i = 0;
             foreach (Student s in students)
             {
+
                 if(s.Age >= 18 && s.Age <= 25)
                 {
                     Console.WriteLine($"Student Name : {s.StudentName.ToString()} and Age :  {s.Age.ToString()}" );
+                    filteredStudents[i] = s;
+                    i++;
+                }
+            }
+            foreach (Student s in filteredStudents.ToList()) 
+            {
+                if (s != null)
+                {
+                    Console.WriteLine($"Student Name :{s.StudentName}  And Age :{s.Age}");
                 }
             }
             // now using LINQ
