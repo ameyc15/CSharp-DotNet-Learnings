1db13f71f8187366170bb77b8c3363af22c32383
ameyc15 <ameychaudhary@yahoo.com>
2026-08-03 13:45:57 +0530

    linq query syntax part 1 completed

---
+++ CSharp-DotNet-Masterclass/Day30_LINQ_1/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day30_LINQ_1
+{
+    internal class Employee
+    {
+        //  id first name, lastname,email, department, salary , hiredate
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime HireDate { get; set; }
+
+    }
+}
--- CSharp-DotNet-Masterclass/Day30_LINQ_1/Program.cs
+++ CSharp-DotNet-Masterclass/Day30_LINQ_1/Program.cs
@@ -89,13 +89,94 @@
                 select t;
             Console.WriteLine(string.Join(",",tutorials));
 
-            // find the friends who has 'a' in their names
+            // find the friends who has 'a' in their names and length<5 ordered by descending
             string[] friendnames = { "Amey", "Mikhail", "Adam", "David", "Bobby", "Christopher" };
             var friends  = from friend in friendnames
-                           where friend.ToLower().Contains("a")
+                           where friend.ToLower().Contains("a") && friend.Length <5
+                           orderby friend descending
                            select friend;
 
             Console.WriteLine(string.Join("," , friends));
+
+            // **********************Exercise**********************
+            List<Employee> employees = new List<Employee>() 
+            {
+                new Employee{Id=1, FirstName="John", LastName="Doe",Email="john@email.com",Department="IT",Salary=60000},
+                new Employee{Id=2,FirstName="Jane", LastName="Smith",Email="jane@email.com",Department="HR",Salary=35000},
+                new Employee{Id=3,FirstName="Robert",LastName="Johnson",Email="rob@email.com",Department="Finance",Salary=85000},
+                new Employee{Id=4,FirstName="Emily",LastName="Davis",Email="emily@gmail.com",Department="IT",Salary=90000},
+                new Employee{Id=5,FirstName="Michael",LastName="Brown",Email="mich@email.com",Department="IT",Salary=87000},
+                new Employee{Id=6,FirstName="Sarah",LastName="Wilson",Email="sarah@email.com",Department="HR",Salary=45000}
+            };
+            // Employee class has data of employee 
+            // 1. Get All employee data from IT department
+            var resultOne = from emp in employees
+                            where emp.Department == "IT"
+                            select emp;
+            foreach (var item in resultOne)
+            {
+                Console.WriteLine($"Id : {item.Id} {item.FirstName} {item.LastName}  {item.Department}  {item.Email}  {item.Salary}");
+            }
+            // 2. Emplpyee Salary greater than 50000
+            Console.WriteLine("*************2nd Question**************");
+            var resultTwo  = from emp in employees
+                             where emp.Salary>50000
+                             select emp;
+            foreach (var item in resultTwo)
+            {
+                Console.WriteLine($"Id : {item.Id}");
+            }
+
+            // 3. Employee from IT and salary > 80000
+            Console.WriteLine("*************3rd Question**************");
+
+            var resultThree = from emp in employees
+                              where emp.Department == "IT" && emp.Salary > 80000
+                              select emp;
+            foreach (var item in resultThree) 
+            {
+               Console.WriteLine(item.FirstName + item.LastName);
+            }
+
+
+
+            // 4. Sort Employee by name 
+            Console.WriteLine("*************4th Question**************");
+
+            var resultfour = from e in employees
+                             orderby e.FirstName
+                             select e;
+            foreach(var item in resultfour)
+            {
+                Console.WriteLine(item.FirstName);
+            }
+
+
+
+
+
+            // 5. Sort by Department Name then Salary High to Low
+            Console.WriteLine("*************5th Question**************");
+            var resultFive = from e in employees
+                             orderby e.Department, e.Salary descending                             
+                             select e;
+           
+            foreach (var item in resultFive)
+            {
+                Console.WriteLine(item.FirstName + item.Salary + item.Department);
+            }
+
+
+            // 6. Employee whose salary between 30k and 60k
+            Console.WriteLine("*************6th Question**************");
+            var resultSix = from e in employees
+                            where e.Salary >= 30000 && e.Salary <= 60000
+                            select e;
+
+            foreach(var item in resultSix)
+            {
+                Console.WriteLine(item.FirstName + item.LastName + item.Salary);
+            }
         }
     }
 }
