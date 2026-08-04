47421ef6ca7c2cd48e0112c989f56305d8393616
ameyc15 <ameychaudhary@yahoo.com>
2026-08-04 09:14:52 +0530

    Linq Part 2 Complete

---
+++ CSharp-DotNet-Masterclass/Day32_LINQ_Method_Syntax_3/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day32_LINQ_Method_Syntax_3
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public decimal Salary { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ CSharp-DotNet-Masterclass/Day32_LINQ_Method_Syntax_3/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace Day32_LINQ_Method_Syntax_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>()
+           {
+               new Employee
+            {
+                Id = 1,Name = "Aarav Sharma",Department = "IT", City = "Pune", Salary = 95000
+            },
+            new Employee
+            {
+                Id = 2,Name = "Priya Deshmukh",Department = "HR",City = "Mumbai", Salary = 65000
+            },
+            new Employee
+            {
+                Id = 3,Name = "Rohan Verma",Department = "IT",City = "Bengaluru",Salary = 120000
+            },
+            new Employee
+            {
+                Id = 4,Name = "Ananya Iyer",Department = "Finance",City = "Pune",Salary = 82000
+            },
+            new Employee
+            {
+                Id = 5,Name = "Vikram Patil",Department = "Marketing",City = "Pune",Salary = 75000
+            },
+            new Employee
+            {
+                Id = 6,Name = "Sneha Kulkarni", Department = "IT",City = "Hyderabad", Salary = 110000
+            }
+           };
+
+            // filter employees with IT department
+            var itdept = employees.Where(e => e.Department == "IT").ToList();
+            foreach (var item in itdept)
+            {
+                Console.WriteLine("Name " + item.Name + "Department " + item.Department);
+            }
+
+
+
+            // filter employees whose salary is > 100000
+            var filterSal = employees.Where(e => e.Salary > 100000).ToList();
+            foreach (var item in filterSal)
+            {
+                Console.WriteLine(item.Name + item.Salary);
+            }
+
+            // sort employees by salary high to low 
+            Console.WriteLine("**********************Third Query*********************");
+            var sortedSal = employees.OrderByDescending(e => e.Salary).ToList();
+            foreach (var item in sortedSal)
+            {
+                Console.WriteLine(item.Name + item.Salary);
+            }
+
+
+            // Sort by department then sort by Salary descending
+            Console.WriteLine("****************************4th Question****************************");
+            var result = employees
+                         .OrderByDescending(e => e.Department)
+                         .ThenByDescending(e => e.Salary)
+                         .ToList();
+            foreach (var item in result)
+            {
+                Console.WriteLine("Name " + item.Name + " Salary " +item.Salary + " Department " +item.Department);
+            }
+
+            // Group by the department 
+            Console.WriteLine("****************************5th Question****************************");
+            var result1 = employees
+                          .GroupBy(e => e.Department)
+                          .Select
+                          (
+                                g=> new
+                                {
+                                    Department = g.Key,
+                                    Count = g.Count()
+                                }
+                          ).ToList();
+            foreach (var grp in result1)
+            {
+                Console.WriteLine("Department : " + grp.Department + "Count of Employees  " + grp.Count);
+            }
+
+
+        }
+    }
+}
