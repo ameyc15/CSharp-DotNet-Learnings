910c159600661059cff617aadba150ebcbe9bc18
ameyc15 <ameychaudhary@yahoo.com>
2026-08-09 12:30:17 +0530

    ef

---
+++ CSharp-DotNet-Masterclass/Day36_EFCore_1/Controllers/EmployeeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day36_EFCore_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+    }
+}
