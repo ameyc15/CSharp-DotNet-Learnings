883c014fbee936c4a6359ffff285f3ca0a395a06
ameyc15 <ameychaudhary@yahoo.com>
2026-08-09 12:31:50 +0530

    ef

---
+++ CSharp-DotNet-Masterclass/Day36_EFCore_1/Repositories/EmployeeReposiotory.cs
@@ -0,0 +1,7 @@
+﻿namespace Day36_EFCore_1.Repositories
+{
+    public class EmployeeReposiotory
+    {
+
+    }
+}
---
+++ CSharp-DotNet-Masterclass/Day36_EFCore_1/Services/EmployeeService.cs
@@ -0,0 +1,6 @@
+﻿namespace Day36_EFCore_1.Services
+{
+    public class EmployeeService
+    {
+    }
+}
