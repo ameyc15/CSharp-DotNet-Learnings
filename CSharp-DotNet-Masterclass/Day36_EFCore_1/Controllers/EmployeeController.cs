6f9be25c20664230c7b8ebd0d8fcd53aca042169
ameyc15 <ameychaudhary@yahoo.com>
2026-08-09 12:36:19 +0530

    ef

--- CSharp-DotNet-Masterclass/Day36_EFCore_1/Controllers/EmployeeController.cs
+++ CSharp-DotNet-Masterclass/Day36_EFCore_1/Controllers/EmployeeController.cs
@@ -7,5 +7,27 @@ namespace Day36_EFCore_1.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
+        public EmployeeController() 
+        {
+
+        }
+        // Get : api/employee/all
+        [HttpGet("get-all-employees")]
+        public  async Task<IActionResult> GetAllEmployees()
+        {
+            return Ok();
+        }
+
+        [HttpGet("get-employee-by-id")]
+        public async Task<IActionResult> GetEmployeeById()
+        {
+            return Ok();
+        }
+
+        [HttpPost("create-employee")]
+        public async Task<IActionResult> CreateEmployee()
+        {
+            return Ok();
+        }
     }
 }
--- CSharp-DotNet-Masterclass/Day36_EFCore_1/Repositories/EmployeeReposiotory.cs
+++ CSharp-DotNet-Masterclass/Day36_EFCore_1/Repositories/EmployeeReposiotory.cs
@@ -2,6 +2,9 @@
 {
     public class EmployeeReposiotory
     {
+        public EmployeeReposiotory() 
+        {
 
+        }
     }
 }
