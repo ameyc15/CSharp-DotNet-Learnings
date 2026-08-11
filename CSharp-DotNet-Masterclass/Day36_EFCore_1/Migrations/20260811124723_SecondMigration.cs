bfdce3aaeb9d9d1f0b782849641a75797340de3b
ameyc15 <ameychaudhary@yahoo.com>
2026-08-11 18:29:32 +0530

    Second Migration using Attributes

---
+++ CSharp-DotNet-Masterclass/Day36_EFCore_1/Migrations/20260811124723_SecondMigration.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Day36_EFCore_1.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Employees",
+                newName: "last_name");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Employees",
+                newName: "first_name");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Salary",
+                table: "Employees",
+                type: "decimal(5,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "first_name",
+                table: "Employees",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "last_name",
+                table: "Employees",
+                newName: "LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "first_name",
+                table: "Employees",
+                newName: "FirstName");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Salary",
+                table: "Employees",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(5,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+        }
+    }
+}
--- CSharp-DotNet-Masterclass/Day36_EFCore_1/Models/Employee.cs
+++ CSharp-DotNet-Masterclass/Day36_EFCore_1/Models/Employee.cs
@@ -1,15 +1,36 @@
-﻿namespace Day36_EFCore_1.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Day36_EFCore_1.Models
 {
+    [Table("Employees")]
     public class Employee
     {
+        // Attributes 
+        // Help to define database rules in model c# class
+        // 1. Table - tells EF which table class represents 
+        // 2. Column : if property name is different fromm database column
+        // 3. key : to define which is primary key
+        // 4. Data Validation Enforce :
+        // [Required] required property always
+        // [MaxLength] maximum lenght accpet
+        // [NotMapped] will be excluding in dataabase mapping
+        [Key]
         public int Id { get; set; }
-
+        [Required]
+        [MaxLength(20)]
+        [Column("first_name")]
         public string FirstName { get; set; }
 
+        [Column("last_name")]
         public string LastName { get; set; }
 
+        [Column(TypeName ="decimal(5,2)")]
         public decimal Salary { get; set; }
 
         public bool IsActive { get; set; }
+
+        [NotMapped]
+        public string MobileNumber { get; set; }
     }
 }
