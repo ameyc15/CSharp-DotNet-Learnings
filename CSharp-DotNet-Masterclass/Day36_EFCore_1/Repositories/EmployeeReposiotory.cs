6aaef732e0708161a415eb77b73d27f4cf8be2df
ameyc15 <ameychaudhary@yahoo.com>
2026-08-11 10:39:02 +0530

    ef

--- CSharp-DotNet-Masterclass/Day36_EFCore_1/Repositories/EmployeeReposiotory.cs
+++ CSharp-DotNet-Masterclass/Day36_EFCore_1/Repositories/EmployeeReposiotory.cs
@@ -1,17 +1,48 @@
 ﻿using Day36_EFCore_1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
 
 namespace Day36_EFCore_1.Repositories
 {
     public class EmployeeReposiotory
     {
-        public EmployeeReposiotory() 
+        private readonly AppDbContext _appDbContext;
+        public EmployeeReposiotory(AppDbContext appDbContext) 
         {
-
+            _appDbContext = appDbContext;
         }
 
-        public async Task<List<Employee>> GetAll()
+        public async Task<List<Employee>> GetAllAsync()
         {
+            return await _appDbContext.Employees.ToListAsync();
+        }
 
+        public async Task<Employee> GetByIdAsync(int id)
+        {
+            return await _appDbContext.Employees.FindAsync(id);
+        }
+
+        public async Task AddAsync(Employee employee)
+        {
+            _appDbContext.Employees.Add(employee);
+            _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteEmployeeByIdAsync(int id)
+        {
+            var emp = await _appDbContext.Employees.FindAsync(id);
+
+            if(emp != null)
+            {
+                _appDbContext.Employees.Remove(emp);
+                await _appDbContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task Update(Employee employee)
+        {
+            _appDbContext.Employees.Update(employee);
+            await _appDbContext.SaveChangesAsync();
         }
     }
 }
