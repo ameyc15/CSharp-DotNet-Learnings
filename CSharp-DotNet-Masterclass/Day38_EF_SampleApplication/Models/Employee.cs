32f1377a6988f9e6b4414520aff9cd6939948dde
ameyc15 <ameychaudhary@yahoo.com>
2026-08-12 20:14:34 +0530

    Ef - sample application

---
+++ CSharp-DotNet-Masterclass/Day38_EF_SampleApplication/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Day38_EF_SampleApplication.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required]
+        [MaxLength(20)]
+        public string FirstName { get; set; }
+
+
+        [MaxLength(20)]
+        public string LastName { get; set; }    
+
+        public string Email { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
