c8fe9d5325003c10ccd35562dd239ef237d89ff8
ameyc15 <ameychaudhary@yahoo.com>
2026-08-13 13:42:39 +0530

    basic auth

---
+++ CSharp-DotNet-Masterclass/Day40_BasicAuthentication/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day40_BasicAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet("getemployees")]
+        public IActionResult GetEmployees()
+        {
+            return Ok(new
+            {
+                User = "Amey",
+                Salary = 100
+            });
+        }
+    }
+}
