3919baea89b64048af4e283277a8e033c6d36f66
ameyc15 <ameychaudhary@yahoo.com>
2026-08-13 17:57:27 +0530

    encrypted true basic auth completed

---
+++ CSharp-DotNet-Masterclass/Day41_BasicAuthEncrypted/BasicAutherisationHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace Day41_BasicAuthEncrypted
+{
+    public static class BasicAutherisationHelper
+    {
+        public static bool IsAutherised(HttpRequest request)
+        {
+            if (!request.Headers.ContainsKey("Authorization"))
+            {
+                return false;
+            }
+
+            var authHeader = request.Headers["Authorization"].ToString();
+            if(!authHeader.StartsWith("Basic "))
+            {
+                return false ;
+            }
+
+            var encodedValue = authHeader.Substring("Basic".Length).Trim();
+            var decodedValue = Convert.FromBase64String(encodedValue);
+            var credentials = Encoding.UTF8.GetString(decodedValue).Split(":");
+
+            var authHeaderUsername = credentials[0];
+            var authHeaderPassword = credentials[1];
+
+            return authHeaderUsername == "admin" && authHeaderPassword == "admin123";
+        }
+    }
+}
---
+++ CSharp-DotNet-Masterclass/Day41_BasicAuthEncrypted/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day41_BasicAuthEncrypted.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet("getemployee")]
+        public IActionResult Get()
+        {
+            if (!BasicAutherisationHelper.IsAutherised(Request))
+            {
+                return Unauthorized("Please enter valid credentials");
+            }
+
+            return Ok(new
+            {
+                Name = "Amey",
+                Address = "Pune"
+            });
+        }
+    }
+}
