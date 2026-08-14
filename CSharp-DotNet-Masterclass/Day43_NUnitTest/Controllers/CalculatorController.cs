9ce666a959a88a15f1c668995f2dfd523ec7e015
ameyc15 <ameychaudhary@yahoo.com>
2026-08-14 17:19:08 +0530

    nunit start

---
+++ CSharp-DotNet-Masterclass/Day43_NUnitTest/Controllers/CalculatorController.cs
@@ -0,0 +1,25 @@
+﻿using Day43_NUnitTest.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day43_NUnitTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly CalculatorService _calculatorService;
+
+        public CalculatorController(CalculatorService calculatorService)
+        {
+            _calculatorService = calculatorService;
+        }
+
+
+        [HttpGet]
+        public IActionResult Add(int a, int b)
+        {
+            return Ok(_calculatorService.Add(a, b));
+        }
+    }
+}
---
+++ CSharp-DotNet-Masterclass/Day43_NUnitTest/Services/CalculatorService.cs
@@ -0,0 +1,11 @@
+﻿namespace Day43_NUnitTest.Services
+{
+    public class CalculatorService
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
