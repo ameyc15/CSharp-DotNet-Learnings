098755997b09920761e892d9a427b65a215d0c05
ameyc15 <ameychaudhary@yahoo.com>
2026-08-14 08:46:41 +0530

    in progress jwt

---
+++ CSharp-DotNet-Masterclass/Day42_JWTToken/Controllers/DataController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day42_JWTToken.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Success");
+        }
+}
---
+++ CSharp-DotNet-Masterclass/Day42_JWTToken/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Day42_JWTToken.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+
+            if (username=="admin" && password=="admin123")
+            {
+                var token = GenerateToken(username);
+                return Ok(); 
+            }
+            return Unauthorized();
+        }
+
+        private string GenerateToken(string username)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MyScreteKey"));
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, username)
+            };
+
+            var token = new JwtSecurityToken
+            (
+                claims : claims,
+                expires: DateTime.UtcNow.AddMinutes(5),
+                signingCredentials: cred
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
