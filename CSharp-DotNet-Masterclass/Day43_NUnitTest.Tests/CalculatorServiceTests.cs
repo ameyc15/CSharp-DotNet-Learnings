caad096d2f06cc67a4fc56bd6959e3908515c290
ameyc15 <ameychaudhary@yahoo.com>
2026-08-15 08:56:28 +0530

    NUnit Test

---
+++ CSharp-DotNet-Masterclass/Day43_NUnitTest.Tests/CalculatorServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Day43_NUnitTest.Services;
+namespace Day43_NUnitTest.Tests
+
+{
+    public class CalculatorServiceTests
+    {
+        private CalculatorService _calculatorService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _calculatorService = new CalculatorService();
+        }
+
+        [Test]
+        public void AddShouldReturnCorrectSum()
+        {
+            // arrange
+            int a = 2, b = 3;
+
+            // act
+            var result = _calculatorService.Add(a, b);
+
+            // assert
+
+            Assert.That(5, Is.EqualTo(result));
+        }
+    }
+}
