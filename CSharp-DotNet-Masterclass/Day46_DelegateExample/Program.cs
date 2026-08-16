4f460ee93c935cf03806a030bd78b96fc46e8588
ameyc15 <ameychaudhary@yahoo.com>
2026-08-16 19:53:31 +0530

    delegatee start

---
+++ CSharp-DotNet-Masterclass/Day46_DelegateExample/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day46_DelegateExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
